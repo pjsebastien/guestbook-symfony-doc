1393c8c2e8b2ae062135b07b0255d2d894884191
pierrejean <pj.sebastien@gmail.com>
2021-07-01 09:27:22 +0400

    Update admin list filter

--- src/Controller/Admin/ConferenceCrudController.php
+++ src/Controller/Admin/ConferenceCrudController.php
@@ -4,6 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Conference;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class ConferenceCrudController extends AbstractCrudController
 {
@@ -12,12 +17,33 @@ class ConferenceCrudController extends AbstractCrudController
         return Conference::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Conference')
+            ->setEntityLabelInPlural('Conferences')
+            ->setSearchFields(['city', 'year', 'is_international'])
+            ->setDefaultSort(['year' => 'DESC']);
+        ;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('city')
+            ->add('year')
+            ->add('isInternational')
+        ;
+    }
+
     
     public function configureFields(string $pageName): iterable
     {
-        return [
-            
-        ];
+        yield TextField::new('city');
+        yield TextField::new('year');
+        yield BooleanField::new('is_international');
+        
+
     }
     
 }
